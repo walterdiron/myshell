c21ce898814a3bb213dad317e0bd4e79c8849453
walterdiron <walterdiron@gmail.com>
2022-08-04 01:51:38 -0300

    Backap

--- Backap.sh
+++ Backap.sh
@@ -1,7 +1,11 @@
-#Variables
-HOME=$HOME
-backap-termux=$HOME/Backap-termux
 clear
 cd
 mkdir Backap-termux
-mkdir backap-termux hola
+rm -r /data/data/com.termux/files/usr/etc/motd-playstore
+rm -r /data/data/com.termux/files/usr/etc/motd-playstore.dkg-old
+rm -r /data/data/com.termux/files/usr/etc/motd.sh
+cp /data/data/com.termux/files/usr/etc/bash.bashrc $HOME/Backap-termux
+cd
+cp .shortcuts Backap-termux/
+zip 
+echo '.:Backap terminado:.'
--- Instalar-herramientas.sh
+++ Instalar-herramientas.sh
@@ -2,15 +2,6 @@
 clear
 cd
 
-#Actualizar
-apt update
-apt full-upgrade -y
-apt autoremove -y
-apt clean
-echo 'A C T U A L I Z A C I O N   C O M P L E T A'
-echo '  Espere que se instalen los programas'
-sleep 10
-
 #Instalar herramientas
 pkg install wget -y
 pkg install git -y
