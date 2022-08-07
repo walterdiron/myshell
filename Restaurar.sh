62e3102e7e748047bc241bd68591b03854a345b3
walterdiron <walterdiron@gmail.com>
2022-08-07 15:54:09 -0300

    Restaurar

--- Restaurar.sh
+++ Restaurar.sh
@@ -1,1 +1,7 @@
 cd
+#Restaurar Bashrc
+rm /data/data/com.termux/files/usr/etc/bash.bashrc
+cp termuxwall/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc
+
+
+echo '. . . T o d o   L i s t o . . .'
