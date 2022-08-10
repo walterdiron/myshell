1395dcbf448211e6c0ea3129eb7793b7213025fa
Walter Diron <70548864+walterdiron@users.noreply.github.com>
2022-08-10 10:47:55 -0300

    Update Instalar-herramientas.sh
    Update

--- Instalar-herramientas.sh
+++ Instalar-herramientas.sh
@@ -1,6 +1,6 @@
 #Posicionar
-clear
 cd
+clear
 
 #Instalar herramientas
 pkg install wget -y
@@ -9,7 +9,7 @@ pkg install cmatrix -y
 pkg install hollywood -y
 pkg install openssh -y
 pkg install htop -y
-pkg install termux-api -y
+pkg install termux-api
 pkg install man -y
 pkg install figlet -y
 pkg install zip -y
@@ -17,42 +17,7 @@ pkg install exa -y
 
 echo '. . . Paquete de herramientas instaladas . . .'
 
-#Aviso de finalizacion del script
 termux-vibrate
 termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-termux-vibrate
-echo '          . . . T O D O  L I S T O . . .'
 termux-setup-storage
+echo '          . . . T O D O  L I S T O . . .'
