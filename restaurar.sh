c360a07349b18720abc34bb7abdaed873b91a54d
walterdiron <walterdiron@gmail.com>
2022-09-10 12:27:21 -0300

    cambio

--- restaurar.sh
+++ restaurar.sh
@@ -4,6 +4,7 @@ cd
 #Restaurar Bashrc
 rm /data/data/com.termux/files/usr/etc/bash.bashrc
 cp termuxwall/bash.bashrc /data/data/com.termux/files/usr/etc/bash.bashrc
+
 source /data/data/com.termux/files/usr/etc/bash.bashrc
 
 
