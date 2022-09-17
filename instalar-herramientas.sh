6289500d17abd588fe73603a65218d834f8deda9
walterdiron <walterdiron@gmail.com>
2022-09-17 11:53:00 -0300

    servidor ssh

--- instalar-herramientas.sh
+++ instalar-herramientas.sh
@@ -7,7 +7,6 @@ pkg install zsh -y
 pkg install wget -y
 pkg install cmatrix -y
 pkg install hollywood -y
-pkg install openssh -y
 pkg install htop -y
 pkg install termux-api
 pkg install man -y
---
+++ servidor-ssh.sh
@@ -0,0 +1,11 @@
+cd
+apt install openssh -y
+apt install net-tools -y
+apt install procps -y
+
+sshd
+passwd
+diron7547
+diron7547
+ifconfig
+echo 'Servidor instalado recordar que el puerto es 8022'
