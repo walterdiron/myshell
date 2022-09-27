4f338b894800d3a5819815f118abae0d4e6485f6
walterdiron <walterdiron@gmail.com>
2022-09-27 12:41:26 -0300

    hoal

--- instalar-herramientas.sh
+++ instalar-herramientas.sh
@@ -5,6 +5,8 @@ clear
 #Instalar herramientas
 pkg install zsh -y
 pkg install wget -y
+pkg install curl -y
+pkg install file -y
 pkg install cmatrix -y
 pkg install hollywood -y
 pkg install htop -y
@@ -14,4 +16,5 @@ pkg install figlet -y
 pkg install zip -y
 pkg install exa -y
 termux-setup-storage -y
+
 echo '. . . Paquete de herramientas instaladas . . .'
