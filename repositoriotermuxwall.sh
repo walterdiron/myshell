d976c8a8b4045ecbe0c751ec7ded787f4c906448
Walter Diron <70548864+walterdiron@users.noreply.github.com>
2022-09-29 11:10:31 -0300

    Update repositoriotermuxwall.sh
    Actualizando

--- repositoriotermuxwall.sh
+++ repositoriotermuxwall.sh
@@ -1,7 +1,10 @@
 #Actualizando repositorio TERMUXWALL.
 cd
-rm -r -f -d termuxwall
+rm -r -f -d .baulera/termuxwall
+cd .baulera
 git clone https://github.com/walterdiron/termuxwall.git
+cd
+chmod +x .baulera/termuxwall/*
 
 echo '. : R E P O S I T O R I O   A C T U A L I Z A D O : .'
 cd
